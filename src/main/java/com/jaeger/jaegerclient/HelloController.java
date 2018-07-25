30c20dc68b565a9f88eb1c343e67144112facb1f
punki03 <tonny_jho@hotmail.com>
2018-07-25 17:18:00 -0500

    Agregadas modificaciones a HelloController(Peticion chaining) y JaegerClientApplication (RestTemplate) Bean

--- src/main/java/com/jaeger/jaegerclient/HelloController.java
+++ src/main/java/com/jaeger/jaegerclient/HelloController.java
@@ -1,16 +1,37 @@
 package com.jaeger.jaegerclient;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
-    @RequestMapping("/hello")
-    public String hello(){
+	
+	@Autowired
+	RestTemplate restTemplate;
+	
+	@RequestMapping("/")
+	public String index() {
+		return "Index servicio uno";
+	}
+	
+    @SuppressWarnings("unused")
+	@RequestMapping("/hello")
+    public String hello() throws Exception{
+    	if(true) {
+			throw new Exception();
+    	}
         return "Hola desde Controller";
     }
 
+    @RequestMapping("/chaining")
+    public String chaining() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8081/dos", String.class);
+        return "Chaining + " + response.getBody();
+    }
 
 
 }
--- src/main/java/com/jaeger/jaegerclient/JaegerClientApplication.java
+++ src/main/java/com/jaeger/jaegerclient/JaegerClientApplication.java
@@ -4,10 +4,18 @@ import io.jaegertracing.Configuration;
 import io.opentracing.Tracer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class JaegerClientApplication {
+	
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
+		return restTemplateBuilder.build();
+	}
 
 	@Bean
 	public Tracer jaegerTracer(){
